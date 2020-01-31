48e2c981827c02a2beca3ac9e6a3c4b3031a8f49
Emily ou <emilyou34@gmail.com>
2020-01-31 16:18:53 -0500

    Added collection view setup and layout - Flix part 2 done without bonus

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Emily Ou on 1/31/20.
+//  Copyright © 2020 Emily Ou. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
